6ebbd2e4c85849b767c459a3fa2cdc6f00f34b60
hasfsghj <plescaericamaria@gmail.com>
2024-07-24 15:30:57 +0300

    mata si caine

--- src/views/BlogList.vue
+++ src/views/BlogList.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <h1>Blogul GGIT FD</h1>
+        <h1>BUNA!!!
+            Blogul GGIT FD</h1>
+
+            <img href="https://i3.cpcache.com/merchandise_zoom/115741933/124_750x750_Front_Color-NA.jpg?region={%22name%22:%22FrontCenter%22,%22width%22:2.6950095,%22height%22:2.7,%22alignment%22:%22MiddleCenter%22,%22orientation%22:0,%22dpi%22:300,%22crop_x%22:0,%22crop_y%22:0,%22crop_h%22:540,%22crop_w%22:539,%22scale%22:0.99815154,%22template%22:{%22id%22:115741933,%22params%22:{}}}&AttributeValue=NA&c=False&cid=PUartJBjiF%2Fyg4FdKqiggQ%3D%3D+%7C%7C+yPvjkVsGsggUtyS84ab0YQ%3D%3D&ProductNo=436845707">
+            <img href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYIFwO6XDBdicf6P_BD8WicDN8plv08OrL2g&s">
 
         <div v-if="blogList.length>0">
             <BlogItem v-for="blog in blogList"
@@ -92,5 +96,10 @@ import {myaxios} from '../axios'
 </script>
 
 <style scoped>
-
+h1 {
+color:rgb(69, 30, 14)
+}
+body {
+background-color:burlywood
+}
 </style>
